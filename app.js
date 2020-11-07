2b73a1ae5fa1d5b837d53551f877d4ce1a782810
Ryo Chikazawa <chikathreesix@gmail.com>
2020-11-08 02:20:43 +0900

    Use port from env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const totp = require('totp-generator')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({
     extended: true
