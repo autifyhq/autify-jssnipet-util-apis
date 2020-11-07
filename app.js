301768bdb8d8b29cfc787e8ea5860b925d67291b
Ryo Chikazawa <chikathreesix@gmail.com>
2020-11-08 02:22:55 +0900

    CORS: Allow all

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
-app.options('*', cors())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send("Hello World")
