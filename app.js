ca3fd03ad2e1243eb365012ebd1842c9defbf986
Ryo Chikazawa <chikathreesix@gmail.com>
2020-11-08 02:18:43 +0900

    Add cars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const totp = require('totp-generator')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -8,6 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
+app.options('*', cors())
 
 app.get('/', (req, res) => {
   res.send("Hello World")
